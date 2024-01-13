a205bd4fbe68969c737c3f48a91c74f3e7b349c2
zahid-01 <erzahidkhan101@gmail.com>
2024-01-13 09:40:37 +0530

    BSearch recursive

--- ArrayADT/ArrayADT.cpp
+++ ArrayADT/ArrayADT.cpp
@@ -3,21 +3,25 @@
 
 using namespace std;
 
-struct ADTArray {
+struct ADTArray
+{
 	int A[10];
 	int size;
 	int length;
 };
 
-void display(ADTArray arr) {
+void display(ADTArray arr)
+{
 	for (int i = 0; i < arr.length; i++)
 		cout << arr.A[i] << " ";
 
 	cout << endl;
 }
 
-void insert(ADTArray *arr, int el, int index) {
-	if (index >= 0 && index <= arr->length-1) {
+void insert(ADTArray *arr, int el, int index)
+{
+	if (index >= 0 && index <= arr->length - 1)
+	{
 		for (int i = arr->length; i > index; i--)
 			arr->A[i] = arr->A[i - 1];
 
@@ -26,22 +30,70 @@ void insert(ADTArray *arr, int el, int index) {
 	}
 }
 
-void delElement(ADTArray *arr, int index) {
-	if (index >= 0 && index < arr->length) {
-		for (int i = index; i < arr->length; i++) 
+void delElement(ADTArray *arr, int index)
+{
+	if (index >= 0 && index < arr->length)
+	{
+		for (int i = index; i < arr->length; i++)
 			arr->A[i] = arr->A[i + 1];
 		arr->length--;
 	}
 }
 
+int lSearch(ADTArray arr, int el)
+{
+	for (int i = 0; i < arr.length; i++)
+		if (arr.A[i] == el)
+			return i;
+
+	return -1;
+}
+
+int lSearchOpt(ADTArray *arr, int el)
+{
+	for (int i = 0; i < arr->length; i++)
+	{
+		if (arr->A[i] == el)
+		{
+			int temp = arr->A[i];
+			arr->A[i] = arr->A[i - 1];
+			arr->A[i - 1] = temp;
+
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int BSearch(ADTArray arr, int el, int p, int q)
+{
+	if (p > q)
+		return -1;
+
+	int mid = (p + q) / 2;
+
+	if (arr.A[mid] == el)
+		return mid;
+
+	if (arr.A[mid] > el)
+		return BSearch(arr, el, p, mid - 1);
+	else
+		return BSearch(arr, el, mid + 1, q);
+}
+
 int main()
 {
-	ADTArray Arr = { {5,6,7,1,2}, 10, 5};
+	ADTArray Arr = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 2000}, 10, 10};
 	display(Arr);
 
-	insert(&Arr, 200, 4);
-	display(Arr);
-	
-	delElement(&Arr, 6);
-	display(Arr);
+	// insert(&Arr, 200, 4);
+	// delElement(&Arr, 6);
+
+	// cout << lSearch(Arr, 2)<< endl;
+
+	// lSearchOpt(&Arr, 1);
+
+	cout << BSearch(Arr, 20000, 0, 9);
+	// display(Arr);
 };
--- Sum ofN-Natural numbers/Main.cpp
+++ Sum ofN-Natural numbers/Main.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int sum_N_Numbers(int n) {
-	if (n <= 0) return 0;
+	if (n == 1) return 1;
 
 	return n + sum_N_Numbers(n - 1);
 }
 
 int main() {
-	//cout << "Sum of first 5 natural numbers is " << sum_N_Numbers(1000000) << endl;
-	int counter = 0;
-	for (int i = 0; i < 1000000; i++)
-		counter += i;
+	cout << "Sum of first 5 natural numbers is " << sum_N_Numbers(5) << endl;
+	// int counter = 0;
+	// for (int i = 0; i < 1000000; i++)
+	// 	counter += i;
 
-	cout << counter << endl;
+	// cout << counter << endl;
 	return 0;
 }
